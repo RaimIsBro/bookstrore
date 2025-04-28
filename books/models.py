2e6c4601afb71d0321ecb39e1b4d31538aaa8bce
RaimIsBro <raimraim806@gmail.com>
2025-04-28 09:33:22 +0500

    Добавлена модель Publisher в приложение books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
